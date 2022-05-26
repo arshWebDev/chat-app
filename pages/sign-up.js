c813be67fe24b3a25e194394bec9676d4a822306
arshWebDev <arshgoyalDev@gmail.com>
2022-05-26 10:52:09 +0530

    styled for darkMode

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -106,7 +106,7 @@ const SignUp = () => {
 
           <button className="grid place-items-center w-full py-3 bg-primary rounded-lg mt-10">
             {loading ? (
-              <div className="w-4 h-4 border-t-2 border-l-2 border-white border-solid animate-spin rounded-full"></div>
+              <div className="w-5 h-5 border-t-2 border-l-2 border-white border-solid animate-spin rounded-full"></div>
             ) : (
               <span className="text-white font-semibold">Create Account</span>
             )}
